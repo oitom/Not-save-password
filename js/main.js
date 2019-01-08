528b30eb05193dcddcc2796480c54ece67c6f96f
Wellington Costa <wcosta.ale@gmail.com>
2019-01-08 10:59:19 -0200

    Atualização do carregamento da font password

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ $("#pass").keyup(function() {
 	else	
 		$(this).css('font-family', 'password');
 });
+
+$("#pass").focus(function() { 
+	if($(this).val().length == 0)
+		$(this).css('font-family', 'inherit');
+	else	
+		$(this).css('font-family', 'password');
+});
+
+$(document).ready(function() {
+	$("#pass").focus();
+	
+	setTimeout(function(){ 
+		$("#email").focus();
+	},10);
+});
